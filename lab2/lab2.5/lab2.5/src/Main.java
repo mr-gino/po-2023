35c8af79468bff892f69e1df74d152417e43e624
Dawid <ds127770@stud.ur.edu.pl>
2023-03-19 01:55:35 +0100

    19.03

---
+++ lab2/lab2.5/lab2.5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 20; i >= 0 ; i--) {
+            if (i == 2 || i ==6 || i ==9 || i ==15 || i==19)
+                continue;
+            System.out.printf(i + ", ");
+        }
+    }
+}
