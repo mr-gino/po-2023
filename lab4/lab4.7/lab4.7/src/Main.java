217851cc0a4e79a2892e73efc8dc22ed8fa4b68a
Dawid <ds127770@stud.ur.edu.pl>
2023-03-25 00:42:05 +0100

    25.03

--- lab4/lab4.7/lab4.7/src/Main.java
+++ lab4/lab4.7/lab4.7/src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
-    public static boolean spr(int n, String[] tab1, String[] tab2){
+    public static boolean spr(int n, String[] tab1, String[] tab2) {
         for (int i = 0; i < n; i++) {
-            if (tab1[i].length() == tab2[i].length()){
-                for (int j = 0; j < tab1[i].length(); j++) {
-                    if (tab1[i].charAt(j) != tab2[i].charAt(j) )
+            if (tab1[i].length() == tab2[i].length()) {
+                for (int j = 0; j < tab1[i].length(); j++)
+                    if (tab1[i].charAt(j) != tab2[i].charAt(j))
                         return false;
-                }
             } else return false;
         }
         return true;
     }
+
     public static void main(String[] args) {
         int n = 5;
         String[] tab1 = {"Ala", "ma", "kota", "i", "psa"};
         String[] tab2 = {"Ala", "ma", "kota", "i", "psa"};
-        System.out.println("Podane tablice " + (spr(n, tab1, tab2) ? "są" : "nie są")+ " takie same");
+        System.out.println("Podane tablice " + (spr(n, tab1, tab2) ? "są" : "nie są") + " takie same.");
 
     }
 }
